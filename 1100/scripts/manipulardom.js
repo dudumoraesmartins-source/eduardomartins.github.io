065c629ce930b81501b4fc7bb6c639cb33cad3ad
Eduardo Moraes Martins <dudumoraesmartins@yahoo.com.br>
2026-02-02 20:22:18 -0300

    commit_inicial

---
+++ 1100/scripts/manipulardom.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('nav');
+const produtos = document.querySelector('.experiencia a');
+const homeLink = document.querySelector('#menu li a');
+
+console.log(nav);
+console.log(produtos);
+console.log(homeLink);
+
+console.dir(nav);
---
+++ 1100/scripts/teste.js
@@ -0,0 +1,8 @@
+const Nomee = 'Eduardo Const';
+/* const não permite alterar seu valor */
+
+
+let nome = 'Eduardo Let';
+nome = 'Eduardo permite alterar valor com let';
+
+console.log(nome);
